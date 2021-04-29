f2817816c21b1a42176b5e3da1978850bdc8a938
kuduzovkhamzat@gmail.com <khamzat995@gmail.com>
2021-04-29 15:38:02 +0300

    server-first

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.write("Привет мир")
+  res.end();
+})
+
+server.listen(3000);
+
+console.log(4)
