a01ad5487b6f79cfa27f02f73e5d041baca4d77e
kuduzovkhamzat@gmail.com <khamzat995@gmail.com>
2021-04-29 16:06:49 +0300

    serverTwo

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const http = require('http');
+const path = require('path');
+
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.write("Привет мир")
+  res.writeHead(200, {
+    'Content-type': 'text/html'
+  });
+  res.write('<h1>Hello from intocode coding bootcamp</h1>')
   res.end();
 })
 
 server.listen(3000);
 
-console.log(4)
+//Задача 2.1. на половину
